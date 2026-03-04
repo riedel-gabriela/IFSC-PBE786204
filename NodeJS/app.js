7cdf1c42c213337422805066ff0d19b4f5009d4b
riedel-gabriela <gabriela.r29@aluno.ifsc.edu.br>
2026-03-03 22:12:23 -0300

    added init node app

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/data', (req, res) => {
+  const date = new Date();
+  const formatted_date = format(date, 'dd/MM/yyyy')
+  res.send(formatted_date)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
